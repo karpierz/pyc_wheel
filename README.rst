1ac5aa2a23e1ba7dd8dad0b15c3dda26bc4f933e
Adam Karpierz <akarpierz@gmail.com>
2020-09-22 20:20:07 +0200

    1.2.1 (2020-09-22) ------------------ - Drop support for Python 3.5.

--- README.rst
+++ README.rst
@@ -42,7 +42,7 @@ Installation
 
 Prerequisites:
 
-+ Python 3.5 or higher
++ Python 3.6 or higher
 
   * https://www.python.org/
   * 3.7 is a primary test environment.
--- src/pyc_wheel/__about__.py
+++ src/pyc_wheel/__about__.py
@@ -9,7 +9,7 @@ __all__ = ('__title__', '__summary__', '__uri__', '__version_info__',
 __title__        = "pyc_wheel"
 __summary__      = "Compile all py files in a wheel to pyc files."
 __uri__          = "https://pypi.org/project/pyc_wheel/"
-__version_info__ = type("version_info", (), dict(major=1, minor=2, micro=0,
+__version_info__ = type("version_info", (), dict(major=1, minor=2, micro=1,
                         releaselevel="final", serial=0))
 __version__      = "{0.major}.{0.minor}.{0.micro}{1}{2}".format(__version_info__,
                    dict(alpha="a", beta="b", candidate="rc", final="",
