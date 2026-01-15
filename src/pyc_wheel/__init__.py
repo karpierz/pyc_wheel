36d34a1e647b3c980da7f4c94f0e337707ba9be7
Adam Karpierz <akarpierz@gmail.com>
2026-01-16 00:49:36 +0100

    Update documentation

--- src/pyc_wheel/__init__.py
+++
@@ -1,7 +0,0 @@
-# Copyright (c) 2016 Grant Patten
-# Copyright (c) 2019 Adam Karpierz
-# SPDX-License-Identifier: MIT
-
-from .__about__ import * ; del __about__  # type: ignore[name-defined]  # noqa
-
-from ._pyc_wheel import * ; del _pyc_wheel  # type: ignore[name-defined]  # noqa
