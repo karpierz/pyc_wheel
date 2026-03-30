2badca7c1ad85a56b2e2f75b7a1af07f1615dfd4
Adam Karpierz <akarpierz@gmail.com>
2026-03-30 21:04:57 +0200

    Update documentation

--- src/pyc_wheel/__about__.py
+++
@@ -1,5 +0,0 @@
-# Copyright (c) 2016 Grant Patten
-# Copyright (c) 2019 Adam Karpierz
-# SPDX-License-Identifier: MIT
-
-__import__("pkg_about").about()
--- src/pyc_wheel/__main__.py
+++
@@ -1,7 +0,0 @@
-# Copyright (c) 2016 Grant Patten
-# Copyright (c) 2019 Adam Karpierz
-# SPDX-License-Identifier: MIT
-
-import sys
-from ._pyc_wheel import main
-sys.exit(main())
